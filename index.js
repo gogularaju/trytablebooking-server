52ce4e71bf24feaa135b091dab94ee49b80a36ed
gogularaju <gogularaju3@gmail.com>
2021-10-12 17:40:44 +0530

    add node js cors error

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ con.on('close', () => {
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 //Routers
 //app.use('/', TableRoute);
 app.use('/tables', TableRoute);
